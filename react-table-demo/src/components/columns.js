4781af0674ca6b0e4f0237969939631886152691
johan <johanna.pulli@outlook.com>
2023-02-17 08:05:43 +0200

    basictable is set up and reads & displays all data from json

---
+++ react-table-demo/src/components/columns.js
@@ -0,0 +1,35 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First name',
+        accessor: 'firstName'
+
+    },
+    {
+        Header: 'Last name',
+        accessor: 'lastName'
+
+    },
+    {
+        Header: 'Birthday',
+        accessor: 'birthday'
+
+    },
+    {
+        Header: 'Address',
+        accessor: 'address'
+
+    },
+    {
+        Header: 'Favorite color',
+        accessor: 'favoriteColor'
+
+    },
+    {
+        Header: 'Phone',
+        accessor: 'phone'
+    }
+]
